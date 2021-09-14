11e844c0fd3e78a1b16e04e229c501bdfc1a8f8d
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-14 11:49:48 +0700

    maybe

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -70,7 +70,8 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
-    return pairs
+    else:
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="kaguya_back")]]
 
 
 def send_to_list(
